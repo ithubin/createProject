1709d8ce00e76223a86284279d1bc0075946b2e7
ella <ithubin@163.com>
2018-08-02 23:59:50 +0800

    this is 1st commit

---
+++ demo_inxedu_open/src/main/java/com/Demo.java
@@ -0,0 +1,13 @@
+package com;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * Created by on 2018/8/2.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("这是测试");
+    }
+}
